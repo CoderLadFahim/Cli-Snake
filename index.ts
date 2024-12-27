b2df7085ebc63a0146319533f6c3427a032a3c66
CoderLadFahim <fahimalemroz@gmail.com>
2024-12-27 18:25:27 +0600

    Game now functions properly, but vars are misnamed

--- index.ts
+++ index.ts
@@ -7,63 +7,101 @@ const rl = readline.createInterface({
 
 type ValidMove = 'h' | 'j' | 'k' | 'l'
 
+interface CoordinateType {
+	x: number
+	y: number
+}
+
 class InteractiveCanvas {
 	public bgChar: string
 	public mainChar: string
 	public rowCount: number
 	public columnCount: number
-	private pointerCoords: {[key: string]: number}
-	private row: string[]
-	private canvas: Array<string[]>
-	private userInput: ValidMove
 
-	constructor(
-		pointerCoords: {[key: string]: number},
-		rowCount: number = 6,
-		columnCount: number = 10
-	) {
+	private pointerCoords: CoordinateType
+	private row: string[]
+	private userInput: ValidMove
+	private canvas: Array<string[]>
+
+	constructor(rowCount: number = 6, columnCount: number = 10) {
 		this.bgChar = '□'
 		this.mainChar = '■'
 		this.rowCount = rowCount
 		this.columnCount = columnCount
-		this.pointerCoords = pointerCoords
 		this.row = Array(rowCount).fill(this.bgChar)
-		this.canvas = Array(columnCount)
-			.fill(null)
-			.map(() => [...this.row])
+		this.pointerCoords = {x: 0, y: 0}
 
-		this.canvas[this.pointerCoords.x][this.pointerCoords.y] =
-			this.mainChar
+		// rendering the canvas initially
+		this.initializeNewCanvas()
+		this.renderCanvas()
 	}
 
-	private getUserInput() {
+	private getUserInput(): void {
 		rl.question(`Enter move: `, (userInput: ValidMove) => {
-			const validMoves: ValidMove[] = ['h', 'j', 'k', 'l'];
-
-			if (!validMoves.includes(userInput)) {
-			    console.log('Invalid move!');
-			    return this.getUserInput();
-			}
-
-			this.handleValidUserInput();
-
-			rl.close();
+			this.userInput = userInput
+			this.handleUserInput()
 		})
 	}
 
-	private handleValidUserInput() {
-        console.log('that was a valid move!');
+	private handleUserInput(): void {
+		const showOutOfRangeError = () => {
+			console.log('Out of range!')
+			return this.getUserInput()
+		}
+
+		const xCoord = this.pointerCoords.x + 1
+		const yCoord = this.pointerCoords.y + 1
+
+		switch (this.userInput) {
+			case 'h':
+				if (xCoord === 1) return showOutOfRangeError()
+				this.pointerCoords.x--
+				break
+			case 'j':
+				if (yCoord === this.rowCount) return showOutOfRangeError()
+				this.pointerCoords.y++
+				break
+			case 'k':
+				if (yCoord === 1) return showOutOfRangeError()
+				this.pointerCoords.y--
+				break
+			case 'l':
+				if (xCoord === this.columnCount)
+					return showOutOfRangeError()
+				this.pointerCoords.x++
+				break
+			default:
+				console.log('Invalid move!')
+				return this.getUserInput()
+		}
+
+		this.renderCanvas()
+		this.printCanvas()
 	}
 
 	private renderCanvas(): void {
-		console.log(this.canvas)
+		this.initializeNewCanvas()
+		this.canvas[this.pointerCoords.y][this.pointerCoords.x] =
+			this.mainChar
 	}
 
-	public startGame() {
+	private printCanvas(): void {
+		console.log(this.canvas)
+		this.getUserInput()
+	}
+
+	private initializeNewCanvas() {
+		this.canvas = Array(this.columnCount)
+			.fill(null)
+			.map(() => [...this.row])
+	}
+
+	public startGame(): void {
+		this.printCanvas()
 		this.getUserInput()
 	}
 }
 
-const interactiveCanvasInstance = new InteractiveCanvas({x: 1, y: 2})
+const interactiveCanvasInstance = new InteractiveCanvas()
 
 interactiveCanvasInstance.startGame()
