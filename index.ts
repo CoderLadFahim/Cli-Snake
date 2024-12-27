834c53b6de792fb4ed0316e7c65593b5a1f7bc90
CoderLadFahim <fahimalemroz@gmail.com>
2024-12-27 15:17:03 +0600

    Taking user input

--- index.ts
+++ index.ts
@@ -1,26 +1,36 @@
-const canvas = [
-	['■', '■', '■', '■', '■'],
-	['■', '■', '■', '■', '■'],
-	['■', '■', '■', '■', '■'],
-	['■', '■', '■', '■', '■'],
-	['■', '■', '■', '■', '■'],
-	['■', '■', '■', '■', '■'],
-	['■', '■', '■', '■', '■'],
-	['■', '■', '■', '■', '■'],
-	['■', '■', '■', '■', '■'],
-	['■', '■', '■', '■', '■'],
-	['■', '■', '■', '■', '■'],
-]
+const readline = require('node:readline');
 
-const bgChar = '■';
-const mainChar = '☆';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const pointerCoords = {
-    x: 0,
-    y: 0
+const bgChar = '□';
+const mainChar = '■';
+
+const rowCount: number = 6;
+const columnCount: number = 10;
+
+const row: string[] = Array(rowCount).fill(bgChar);
+const canvas: Array<string[]> = Array(columnCount).fill(null).map(() => [...row]);
+
+
+const pointerCoords: {[key: string]: number} = {
+    x: 1,
+    y: 1
 }
 
 canvas[pointerCoords.x][pointerCoords.y] = mainChar;
 
-console.log(canvas);
+type PlayerInputType = 'h' | 'j' | 'k' | 'l';
 
+// const handlePlayerInput = (playerInput: PlayerInputType) => {
+//     const acceptableInputTypes: PlayerInputType[] = ['h', 'j', 'k', 'l'];
+//     if (!acceptableInputTypes.includes(playerInput))
+//         return 'Invalid input bitch!';
+// }
+
+rl.question(`Which way to go bitch?:  `, playerInput => {
+    console.log(canvas);
+    console.log(`Going: ${ playerInput }`);
+});
